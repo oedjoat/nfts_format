e6a6962cecccdf610ae22624c61cc31a2c181460
oedjoat <ogunfoworadolapo@gmail.com>
2022-11-03 23:28:17 +0100

    changed the code to accomodate new headers

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ output_file = 'files/output.csv'
 
 with open(output_file, 'w') as write_file:
     write_to_file = csv.writer(write_file)
-    write_to_file.writerow(['S/N', 'Filename', 'Description', 'Gender', 'Attributes', 'UUID', 'Output File Name'])
+    write_to_file.writerow(['TEAM NAMES','S/N', 'Filename', 'Description', 'Gender', 'Attributes', 'UUID', 'Output File Name'])
 
     with open('files/nft.csv', 'r') as csv_file:
         read = csv.reader(csv_file, delimiter=',')
@@ -13,16 +13,16 @@ with open(output_file, 'w') as write_file:
         data = [i for i in read]  
         for row in data:
             if row[1] and row[2]:
-                sn, file_name, name, description, gender, trait_types, uuid = row[0], row[1], row[2], row[3], row[4], row[5].split(', '), row[-1]
-                name_trait = trait_types[0].split(':')
-                #eye_trait = trait_types[1].split(':')
-                #teeth_trait = trait_types[2].split(':')
-                #clothing_trait = trait_types[3].split(':')
-                #accesories_trait = trait_types[4].split(':')
-                #expression_trait = trait_types[5].split(':')
-                #strength_trait = trait_types[6].split(':')
-                #weakness_trait = trait_types[7].split(':')
-
+                teamname, sn, file_name, name, description, gender, trait_types, uuid = row[0], row[1], row[2], row[3], row[4], row[5], row[6].split(';'), row[-1]
+                hair_trait = trait_types[0].split(': ')
+                eye_trait = trait_types[1].split(': ')
+                teeth_trait = trait_types[2].split(': ')
+                clothing_trait = trait_types[3].split(':')
+                accesories_trait = trait_types[4].split(':')
+                expression_trait = trait_types[5].split(':')
+                strength_trait = trait_types[6].split(':')
+                weakness_trait = trait_types[-1].split(':')
+               
                 nft = {
                     'format' : 'CHIP-0007',
                     'id' : uuid,
@@ -32,43 +32,43 @@ with open(output_file, 'w') as write_file:
                     'gender': gender,
                     'attributes': [
                         {
-                            'trait_type': name_trait[0],
-                            'value': name_trait[1]
+                            'trait_type': hair_trait[0],
+                            'value': hair_trait[1]
                         },
-                        # {
-                        #     'trait_type': eye_trait[0],
-                        #     'value': eye_trait[1]
-                        # },
-                        # {
-                        #     'trait_type': teeth_trait[0],
-                        #     'value': teeth_trait[0]
-                        # },
-                        # {
-                        #     'trait_type': clothing_trait[0],
-                        #     'value': clothing_trait[1]
-                        # },
-                        # {
-                        #     'trait_type': accesories_trait[0],
-                        #     'value': accesories_trait[1]
-                        # },
-                        # {
-                        #     'trait_type': expression_trait[0],
-                        #     'value': expression_trait[1]
-                        # },
-                        # {
-                        #     'trait_type': strength_trait[0],
-                        #     'value': strength_trait[1]
-                        # },
-                        # {
-                        #     'trait_type': weakness_trait[0],
-                        #     'value': weakness_trait[1]
-                        # },
+                        {
+                            'trait_type': eye_trait[0],
+                            'value': eye_trait[1]
+                        },
+                        {
+                            'trait_type': teeth_trait[0],
+                            'value': teeth_trait[1]
+                        },
+                        {
+                            'trait_type': clothing_trait[0],
+                            'value': clothing_trait[1]
+                        },
+                        {
+                            'trait_type': accesories_trait[0],
+                            'value': accesories_trait[1]
+                        },
+                        {
+                            'trait_type': expression_trait[0],
+                            'value': expression_trait[1]
+                        },
+                        {
+                            'trait_type': strength_trait[0],
+                            'value': strength_trait[1]
+                        },
+                        {
+                            'trait_type': weakness_trait[0],
+                            'value': weakness_trait[1]
+                        },
                     ],
                     
-                    'minting_tool' : 'OEDJOAT ######',
+                    'minting_tool' : teamname,
                     'sensitive_content' : False,
                     'series_number' : sn,
-                    'series_total' : data[-1][0],
+                    'series_total' : data[-1][1],
                     'collection' : {
                         'name' : 'Zuri NFT tickets for free lunch',
                         'id' : 'e43fcfe6-1d5c-4d6e-82da-5de3aa8b3b57'
